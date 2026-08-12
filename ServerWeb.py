2cecceac76365a5da1c08f38546b511344210e24
Huantolu <hugo.torres.luna@alumnos.sip.cl>
2026-08-12 10:34:56 -0400

    Limpiar carpetas y enrutamiento

--- ServerWeb.py
+++ ServerWeb.py
@@ -1,9 +1,28 @@
+# Este servidor solo tiene los enrutamientos, back-end lo ves tu Jose
 from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
 
 # Enrutamiento de las páginas web
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/Mapa")
+def map():
+    return render_template("map.html")
+
+@app.route("/Registro")
+def register():
+    return render_template("form-register.html")
+
+@app.route("/Iniciar-sesión")
+def login():
+    return render_template("form-login.html")
+
+@app.route("/Reportes")
+def report():
+    return render_template("report.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
